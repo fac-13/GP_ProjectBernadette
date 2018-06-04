4b2174a1265ecb7764286806dfc45ec7905ed403
Phat Ma <phat.ma@hotmail.co.uk>
2018-06-04 11:37:07 +0100

    create app.js component

---
+++ src/components/app.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import WayFinder from './wayfinder/wayfinder.js';
+
+export default class App extends Component {
+    render() {
+        return <WayFinder />;
+    }
+}
