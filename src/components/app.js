a279d091c8b90ff21deb6dadac6f744a3b72cd7f
Phat Ma <phat.ma@hotmail.co.uk>
2018-06-04 11:51:26 +0100

    WayFinder to Wayfinder

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import WayFinder from './wayfinder/wayfinder.js';
+import Wayfinder from './wayfinder/wayfinder.js';
 
 export default class App extends Component {
     render() {
-        return <WayFinder />;
+        return <Wayfinder />;
     }
 }
