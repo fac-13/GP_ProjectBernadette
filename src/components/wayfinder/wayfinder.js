1dc60eab392b891ff7ee5b7f40acf1156e9a2720
Jennah <contactjennah@gmail.com>
2018-06-04 12:41:23 +0100

    Create initial functionality for wafinder component relates #41

---
+++ src/components/wayfinder/wayfinder.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import data from "./data/wayfinderData.js";
+
+export default class Wayfinder extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      usersPath: ["start"],
+      data: data
+    };
+  }
+
+  questionList = () => {
+    const { data, usersPath } = this.state;
+
+    return usersPath.map(item => {
+      return <QAContainer question={data[item]} key={data[item].question} />;
+    });
+  };
+
+  render() {
+    return <ul>{this.questionList()}</ul>;
+  }
+}
