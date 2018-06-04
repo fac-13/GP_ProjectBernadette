3aaff5ac25a2633dfcd561592261aac1c0a09e08
Phat Ma <phat.ma@hotmail.co.uk>
2018-06-04 17:59:47 +0100

    create wrapper for Wayfinder component Relates #29

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import Wayfinder from './wayfinder/wayfinder.js';
+import styled from 'styled-components';
 
 export default class App extends React.Component {
   render() {
-    return <Wayfinder />;
+    const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    `;
+    return (
+      <Wrapper>
+        <Wayfinder />
+      </Wrapper>
+    );
   }
 }
