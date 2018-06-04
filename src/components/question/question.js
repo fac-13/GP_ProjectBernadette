5cf7a609416e7e8a3e448489541fef7bc037bf23
tspeed90 <tspeed90@gmail.com>
2018-06-04 16:24:50 +0100

    add test to qacontainer component and add test id to question component for testing Relates #45

--- src/components/question/question.js
+++ src/components/question/question.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
 const Question = ({ question }) => {
   return (
     <div>
-      <h2>{question}</h2>
+      <h2 data-testid="question">{question}</h2>
     </div>
   );
 };
