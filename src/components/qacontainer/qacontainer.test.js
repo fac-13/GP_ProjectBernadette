32579b377fa2db49f8d990aa22fea0d3c8e016d0
Phat Ma <phat.ma@hotmail.co.uk>
2018-06-05 14:40:54 +0100

    pass failing tests by providing themeprovider in test files

--- src/components/qacontainer/qacontainer.test.js
+++ src/components/qacontainer/qacontainer.test.js
@@ -2,10 +2,32 @@ import React from 'react';
 import { renderIntoDocument } from 'react-testing-library';
 import QAContainer from './qacontainer';
 import content from '../../data/wayfinderData';
+import { ThemeProvider } from 'styled-components';
+
+// Grandparents Plus brand colors
+const theme = {
+  color: {
+    orange: '#ec632f',
+    yellow: '#fdc800',
+    navy: '#1c2243',
+    blue: '#669dad',
+    white: '#FFF',
+    graydark: '#575756',
+    graymid: '#b2b2b2',
+    graylight: '#dadada'
+  },
+  weight: {
+    lead: '900',
+    link: '600',
+    body: '300'
+  }
+};
 
 test('QA Container renders properly', () => {
   const { getByTestId } = renderIntoDocument(
+    <ThemeProvider theme={theme}>
     <QAContainer qablock={content.start} />
+    </ThemeProvider>
   );
   const question = getByTestId('question');
   expect(question.textContent).toBe('Are you a grandparent?');
--- src/components/question/question.test.js
+++ src/components/question/question.test.js
@@ -1,10 +1,32 @@
 import React from "react";
 import { renderIntoDocument } from "react-testing-library";
 import Question from './question'
+import { ThemeProvider } from 'styled-components';
+
+// Grandparents Plus brand colors
+const theme = {
+  color: {
+    orange: '#ec632f',
+    yellow: '#fdc800',
+    navy: '#1c2243',
+    blue: '#669dad',
+    white: '#FFF',
+    graydark: '#575756',
+    graymid: '#b2b2b2',
+    graylight: '#dadada'
+  },
+  weight: {
+    lead: '900',
+    link: '600',
+    body: '300'
+  }
+};
 
 test("Any question appears", () => {
   const { getByText } = renderIntoDocument(
+    <ThemeProvider theme={theme}>
     <Question question="hello test"/>
+    </ThemeProvider>
   );
   const headerNode = getByText("hello test");
   expect(headerNode.textContent).toBe("hello test");
