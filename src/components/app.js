06cea91e060609259518f99b707a3eb4834cca85
Phat Ma <phat.ma@hotmail.co.uk>
2018-06-05 11:06:01 +0100

    style components for options.js Relates #29

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Wayfinder from './wayfinder/wayfinder.js';
-import styled from 'styled-components';
 
 export default class App extends React.Component {
   render() {
--- src/components/options/options.js
+++ src/components/options/options.js
@@ -1,4 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const OptionsWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+const ButtonsList = styled.ul`
+  padding: 0;
+  margin: 0;
+`
 
 const Button = ({ answer, goto }) => {
   return <button data-goto={goto}>{answer}</button>;
@@ -14,9 +26,9 @@ function Options({ options }) {
   };
 
   return (
-    <div>
-      <ul>{optionList()}</ul>
-    </div>
+    <OptionsWrapper>
+      <ButtonsList>{optionList()}</ButtonsList>
+    </OptionsWrapper>
   );
 }
 
