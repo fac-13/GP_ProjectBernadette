d8bccda01866b0ecd4a74f596f79043988d62df0
Phat Ma <phat.ma@hotmail.co.uk>
2018-06-08 12:46:17 +0100

    change message to template

--- src/data/wayfinderData.js
+++ src/data/wayfinderData.js
@@ -1,3 +1,5 @@
+const alertMsg = "onclick=\"alert('This link will now open in a new tab')\""
+
 const wayfinder = {
   start: {
     question: 'Are you a grandparent?',
@@ -43,17 +45,17 @@ const wayfinder = {
   },
   'info-gen': {
     result:
-      'More information about <a href="https://www.grandparentsplus.org.uk/pages/faqs/category/advice-and-information#" target="_blank" onclick="alert(\'This link will now open in a new window\')">our services</a>',
+      `More information about <a href="https://www.grandparentsplus.org.uk/pages/faqs/category/advice-and-information#" target="_blank" ${alertMsg}>our services</a>`,
     leaf: true
   },
   'info-lost-contact': {
     result:
-      'More information about <a href="https://www.grandparentsplus.org.uk/what-you-can-do" target="_blank" onclick="alert(\'This link will now open in a new window\')">lost or denied contact</a>',
+      `More information about <a href="https://www.grandparentsplus.org.uk/what-you-can-do" target="_blank" ${alertMsg}>lost or denied contact</a>`,
     leaf: true
   },
   'info-kc': {
     result:
-      'More information about being a <a href="https://www.grandparentsplus.org.uk/pages/faqs/category/new-kinship-carer" target="_blank" onclick="alert(\'This link will now open in a new window\')">Kinship Carer here.</a>',
+      `More information about being a <a href="https://www.grandparentsplus.org.uk/pages/faqs/category/new-kinship-carer" target="_blank" ${alertMsg}>Kinship Carer here.</a>`,
     leaf: true
   },
   'legal-order': {
@@ -81,12 +83,12 @@ const wayfinder = {
 
   'order-link-sgo': {
     question:
-      'More information about <a href="https://www.grandparentsplus.org.uk/local-authority-allowances" target="_blank" onclick="alert(\'This link will now open in a new window\')">legal orders</a>.',
+      `More information about <a href="https://www.grandparentsplus.org.uk/local-authority-allowances" target="_blank" ${alertMsg}>legal orders</a>.`,
     options: [{ answer: 'continue', goto: 'complete-form-1' }]
   },
   'order-link-cao': {
     question:
-      'More information about <a href="https://www.grandparentsplus.org.uk/local-authority-allowances" target="_blank" onclick="alert(\'This link will now open in a new window\')">legal orders</a>.',
+      `More information about <a href="https://www.grandparentsplus.org.uk/local-authority-allowances" target="_blank" ${alertMsg}>legal orders</a>.`,
     options: [{ answer: 'continue', goto: 'complete-form-1' }]
   },
 
@@ -113,7 +115,7 @@ const wayfinder = {
   },
   'arrangement-link': {
     question:
-      'More information about <a href="https://www.grandparentsplus.org.uk/informal-arrangements" target="_blank"  onclick="alert(\'This link will now open in a new window\')">informal arrangements</a>.',
+      `More information about <a href="https://www.grandparentsplus.org.uk/informal-arrangements" target="_blank"  ${alertMsg}>informal arrangements</a>.`,
     options: [{ answer: 'continue', goto: 'complete-form-4' }]
   },
 
