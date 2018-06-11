0542965c11f3fb28b8e4d7f8019930aba5bac091
Jennah <contactjennah@gmail.com>
2018-06-11 11:18:29 +0100

    Style thank you component relates #85

--- src/components/thankYou/thankYou.js
+++ src/components/thankYou/thankYou.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const ThankYouStyles = styled.div`
+  background-color: ${props => props.theme.color.yellow};
+  padding: 0% 5%;
+  margin: 0 10%;
+  line-height: 1.3rem;
+`;
 
 const ThankYou = () => {
   return (
-    <div>
+    <ThankYouStyles>
       <p>
         Thank you for submitting the form! You should hear back from us in 3
-        working days.{' '}
+        working days.
+      </p>
+      <p>
         <a href="https://www.grandparentsplus.org.uk/">
           Click here to return to the Grandparents Plus website.
         </a>
       </p>
-    </div>
+    </ThankYouStyles>
   );
 };
 
