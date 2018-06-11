a8fc9d3a27affd1e3b50a60f42084247a2e8d7b0
Jennah <contactjennah@gmail.com>
2018-06-11 10:56:49 +0100

    Add link to gp website relates #85

--- src/components/thankYou/thankYou.js
+++ src/components/thankYou/thankYou.js
@@ -5,7 +5,10 @@ const ThankYou = () => {
     <div>
       <p>
         Thank you for submitting the form! You should hear back from us in 3
-        working days.
+        working days.{' '}
+        <a href="https://www.grandparentsplus.org.uk/">
+          Click here to return to the Grandparents Plus website.
+        </a>
       </p>
     </div>
   );
