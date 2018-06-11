dd3d5af46ccb510c95d40b9afef775031ec94d2b
Jennah <contactjennah@gmail.com>
2018-06-11 10:43:28 +0100

    Create thank you component relates #85

---
+++ src/components/thankYou/thankYou.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ThankYou = () => {
+  return (
+    <div>
+      <p>
+        Thank you for submitting the form! You should hear back from us in 3
+        working days.
+      </p>
+    </div>
+  );
+};
+
+export default ThankYou;
