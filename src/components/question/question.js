33e0d1e19a9eecd6a99376f37db2a14e44c270ff
Jennah <contactjennah@gmail.com>
2018-06-12 15:29:16 +0100

    Add and style svg paths to question.js

--- src/components/question/question.js
+++ src/components/question/question.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
+import leftPath from '../../assets/leftpath.svg';
+import rigthPath from '../../assets/rightpath.svg';
 
-const Div = styled.div`
+const QuestionWrapper = styled.div`
+  width: 250px;
+  margin: 0 auto;
+`;
+
+const StyledDiv = styled.div`
   list-style: none;
   background-color: ${props => props.theme.color.yellow};
   color: ${props => props.theme.color.navy};
@@ -13,23 +20,36 @@ const StyledQuestion = styled.p`
   font-size: 1.5rem;
   font-weight: ${props => props.theme.weight.body};
 `;
+const StyledPathL = styled.img`
+  position: relative;
+  left: 3%;
+`;
+
+const StyledPathR = styled.img`
+  position: relative;
+  right: -21%;
+`;
 
 const Question = ({ question, link, linkText }) => {
   return (
-    <Div>
-      <StyledQuestion data-testid="question">
-        {question}
-        {link ? (
-          <a
-            href={link}
-            target="_blank"
-            onClick={() => alert('This link will now open in a new tab')}
-          >
-            {linkText}
-          </a>
-        ) : null}
-      </StyledQuestion>
-    </Div>
+    <QuestionWrapper>
+      <StyledDiv>
+        <StyledQuestion data-testid="question">
+          {question}
+          {link ? (
+            <a
+              href={link}
+              target="_blank"
+              onClick={() => alert('This link will now open in a new tab')}
+            >
+              {linkText}
+            </a>
+          ) : null}
+        </StyledQuestion>
+      </StyledDiv>
+      <StyledPathL src={`${leftPath}`} alt="a path" />
+      <StyledPathR src={`${rigthPath}`} alt="a path" />
+    </QuestionWrapper>
   );
 };
 
