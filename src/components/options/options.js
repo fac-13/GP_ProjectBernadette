6d76d068ef599d53b3a740f6172d3ea5a7692e77
Jennah <contactjennah@gmail.com>
2018-06-12 16:38:18 +0100

    Change font-size, margin and padding so that design is more responsive and to fix overlapping

--- src/components/options/options.js
+++ src/components/options/options.js
@@ -25,32 +25,27 @@ const ButtonStyle = styled.button`
   border-radius: 5px;
   font-family: ${props =>
     props.children.length > 12 ? 'Raleway, Arial, sans-serif' : 'Evenfall'};
-  font-size: ${props => (props.children.length > 12 ? '1.7rem' : '4rem')};
+  font-size: ${props => (props.children.length > 12 ? '1.1rem' : '2.8rem')};
   font-weight: ${props =>
     props.children.length > 12 ? props.theme.weight.link : ''};
-  margin: ${props => (props.children.length > 12 ? '0 1rem' : '')};
-  padding: ${props => (props.children.length > 12 ? '2.1rem 2rem' : '0 2rem')};
+  margin: ${props =>
+    props.children.length > 12
+      ? '0 1rem'
+      : props.children.length > 10
+        ? '1rem 0.5rem'
+        : '1rem'};
+  padding: ${props =>
+    /* for length greater than 12, the padding is changed for long lines of text i.e SGO
+       for length greater than 10, padding is adjusted for no/notsure, otherwise padding for yes/no */
+    props.children.length > 12
+      ? '.7rem .7rem'
+      : props.children.length > 10
+        ? '0'
+        : '0 2rem'};
   height: 7rem;
   min-width: 4rem;
   min-height: fit-content;
   width: fit-content;
-  @media (max-width: 400px) {
-    font-size: ${props => (props.children.length > 12 ? '1.4rem' : '3.4rem')};
-    min-width: ${props =>
-      props.children.length > 10 ? '3.2rem' : 'fit-content'};
-    padding: ${props =>
-      /* for length greater than 12, the padding is changed for long lines of text i.e SGO
-         for length greater than 10, padding is adjusted for no/notsure, otherwise padding for yes/no */
-      props.children.length > 12
-        ? '.7rem .7rem'
-        : props.children.length > 10
-          ? '0'
-          : '0 2rem'};
-    font-size: ${props => (props.children.length > 12 ? '1.1rem' : '2.8rem')};
-    margin: ${props => (props.children.length > 10 ? '1rem 0.5rem' : '1rem')};
-    min-height: fit-content;
-    width: fit-content;
-  }
 `;
 
 const Button = ({ answer, goto, clickHandler, dataKey, selectedPath }) => {
