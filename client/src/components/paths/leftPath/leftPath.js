7f861d17f544588704b52a605c2a35c643d6f0d1
Phat Ma <phat.ma@hotmail.co.uk>
2018-06-14 18:03:39 +0100

    some fix for zoom ratios

--- client/src/components/paths/leftPath/leftPath.js
+++ client/src/components/paths/leftPath/leftPath.js
@@ -16,10 +16,11 @@ const LeftPath = ({ color = '#DADADA' }) => {
     <StyledPathL>
       <svg
         width="100"
-        height="144"
-        viewBox="0 0 100 144"
+        height="9rem"
+        viewBox="0 0 100 9rem"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
+        preserveAspectRatio="none"
       >
         <path
           d="M50 0H30.2123C30.2123 0 21.6936 12.7407 12.987 53.0864C4.28041 93.4321 0 150.765 0 150.765L39.7104 172C39.7104 172 34.1698 112.543 39.7104 70.0741C45.2509 27.6049 50 0 50 0Z"
